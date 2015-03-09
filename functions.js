3c5034bdd8cce90625e40321f8c1786c06ac4c1b
Greg Santikian <gsantikian@gmail.com>
2015-03-09 10:31:54 -0400

    Adds user input for name and game text

--- functions.js
+++ functions.js
@@ -1,17 +1,24 @@
 var sget = require('sget');
 
-function feedAnimal() {
+function feedAnimal(name) {
 
 }
 
-function putAnimalToSleep() {
+function putAnimalToSleep(name) {
 
 }
 
-function playWithAnimal() {
+function playWithAnimal(name) {
 
 }
 
-function scoldAnimal() {
+function scoldAnimal(name) {
 
 }
+
+console.log("Welcome to Zoo Keeper!");
+var name = sget("A new penguin has arrived at your zoo! What would you like to name it?").trim();
+
+console.log("What would you like to do?\n1 - Feed %s\n2 - Play with %s\n3 - Put %s to sleep\n4 - Scold %s\n5 - Quit game", name, name, name, name);
+var userInput = sget().trim();
+
