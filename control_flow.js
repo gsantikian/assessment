8d44c3967546ee9b48259fe885b4062674c03874
Greg Santikian <gsantikian@gmail.com>
2015-03-09 10:57:08 -0400

    Removes getUserInput function, adds instructions

--- control_flow.js
+++ control_flow.js
@@ -1,9 +1,5 @@
 var sget = require('sget');
 
-function getUserInput() {
-  return parseInt(sget("Please enter a number:"), 10);
-}
-
 function orderNumbers(num1, num2, num3) {
   var numbers = [num1, num2, num3];
   var sortedNumbers = numbers.sort(function(a, b){
@@ -12,7 +8,8 @@ function orderNumbers(num1, num2, num3) {
   return sortedNumbers;
 }
 
-var num1 = getUserInput();
-var num2 = getUserInput();
-var num3 = getUserInput();
+console.log("Please enter 3 numbers.")
+var num1 = parseInt(sget("Enter first number:"),  10);
+var num2 = parseInt(sget("Enter second number:"), 10);
+var num3 = parseInt(sget("Enter third number:"),  10);
 console.log(orderNumbers(num1, num2, num3).join(', '));
