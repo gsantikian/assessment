36e693d187afaae92a1b6c3cda0f2fa0c4ebb013
Greg Santikian <gsantikian@gmail.com>
2015-03-09 11:03:16 -0400

    Cleans up and renames orderNumbers function

--- control_flow.js
+++ control_flow.js
@@ -1,15 +1,14 @@
 var sget = require('sget');
 
-function orderNumbers(num1, num2, num3) {
-  var numbers = [num1, num2, num3];
-  var sortedNumbers = numbers.sort(function(a, b){
+function sortNumbers(num1, num2, num3) {
+  var sortedNumbers = [num1, num2, num3].sort(function(a, b) {
     return b - a;
   });
   return sortedNumbers;
 }
 
-console.log("Please enter 3 numbers.")
-var num1 = parseInt(sget("Enter first number:"),  10);
+console.log("Please enter 3 numbers.");
+var num1 = parseInt(sget("Enter first number:"), 10);
 var num2 = parseInt(sget("Enter second number:"), 10);
-var num3 = parseInt(sget("Enter third number:"),  10);
-console.log(orderNumbers(num1, num2, num3).join(', '));
+var num3 = parseInt(sget("Enter third number:"), 10);
+console.log("Your numbers in decending order: " + sortNumbers(num1, num2, num3).join(', '));
